894b31f9d6675c84cffb085ff26b5f0bc23d2106
raihanahmad01 <dev.raihanahmad@gmail.com>
2020-06-10 23:51:41 +0600

    hamburger manu completed with js and full site completed

--- js/app.js
+++ js/app.js
@@ -12,3 +12,43 @@ $('#navbar a, #footer-link a').on('click', function (event) {
     );
   }
 });
+
+
+// Hamburger Click Event
+// Targeting UI
+const hamburger = document.querySelector('#hamburger');
+const navber = document.querySelector('#navlink');
+
+hamburger.addEventListener('click', function () {
+  hamburger.style.boxShadow = '0 0 2px 4px #7bbbbe';
+
+  if (navber.style.transform === 'scale(1)') {
+    navber.style.transform = 'scale(0)';
+    document.querySelector('#hamburger .b').style.transform = 'unset';
+    document.querySelector('#hamburger .a').style.transform = 'unset';
+    document.querySelector('#hamburger .a').style.top = '20px';
+    document.querySelector('#hamburger .c').style.transform = 'unset';
+    document.querySelector('#hamburger .c').style.top = '44px';
+  } else {
+    navber.style.transform = 'scale(1)';
+    hamburger.style.boxShadow = 'unset';
+    document.querySelector('#hamburger .b').style.transform = 'rotate(225deg)';
+    document.querySelector('#hamburger .a').style.transform = 'rotate(130deg)';
+    document.querySelector('#hamburger .a').style.top = '32px';
+    document.querySelector('#hamburger .c').style.transform = 'rotate(130deg)';
+    document.querySelector('#hamburger .c').style.top = '32px';
+
+    document.querySelector('#navlink').addEventListener('click', function (e) {
+
+      if (e.target.classList.contains('link')) {
+        navber.style.transform = 'scale(0)';
+        document.querySelector('#hamburger .b').style.transform = 'unset';
+        document.querySelector('#hamburger .a').style.transform = 'unset';
+        document.querySelector('#hamburger .a').style.top = '20px';
+        document.querySelector('#hamburger .c').style.transform = 'unset';
+        document.querySelector('#hamburger .c').style.top = '44px';
+      }
+
+    })
+  }
+});
