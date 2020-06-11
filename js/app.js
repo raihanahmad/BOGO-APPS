b58a9434456f36ea24b7a0a906d16e99b6da4eab
raihanahmad01 <dev.raihanahmad@gmail.com>
2020-06-11 09:34:45 +0600

    js code custumize

--- js/app.js
+++ js/app.js
@@ -1,54 +1,55 @@
 // Smooth Scrolling
-$('#navbar a, #footer-link a').on('click', function (event) {
-  if (this.hash !== '') {
+$("#navbar a, #footer-link a").on("click", function (event) {
+  if (this.hash !== "") {
     event.preventDefault();
 
     const hash = this.hash;
 
-    $('html, body').animate({
-        scrollTop: $(hash).offset().top - 0
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 0,
       },
       800
     );
   }
 });
 
-
 // Hamburger Click Event
 // Targeting UI
-const hamburger = document.querySelector('#hamburger');
-const navber = document.querySelector('#navlink');
+const hamburger = document.querySelector("#hamburger"),
+  navber = document.querySelector("#navlink"),
+  lineA = document.querySelector("#hamburger .a"),
+  lineB = document.querySelector("#hamburger .b"),
+  lineC = document.querySelector("#hamburger .c");
 
-hamburger.addEventListener('click', function () {
-  hamburger.style.boxShadow = '0 0 2px 4px #7bbbbe';
+hamburger.addEventListener("click", function () {
+  hamburger.style.boxShadow = "0 0 2px 4px #7bbbbe";
 
-  if (navber.style.transform === 'scale(1)') {
-    navber.style.transform = 'scale(0)';
-    document.querySelector('#hamburger .b').style.transform = 'unset';
-    document.querySelector('#hamburger .a').style.transform = 'unset';
-    document.querySelector('#hamburger .a').style.top = '20px';
-    document.querySelector('#hamburger .c').style.transform = 'unset';
-    document.querySelector('#hamburger .c').style.top = '44px';
+  if (navber.style.transform === "scale(1)") {
+    navber.style.transform = "scale(0)";
+    lineB.style.transform = "unset";
+    lineA.style.transform = "unset";
+    lineA.style.top = "20px";
+    lineC.style.transform = "unset";
+    lineC.style.top = "44px";
   } else {
-    navber.style.transform = 'scale(1)';
-    hamburger.style.boxShadow = 'unset';
-    document.querySelector('#hamburger .b').style.transform = 'rotate(225deg)';
-    document.querySelector('#hamburger .a').style.transform = 'rotate(130deg)';
-    document.querySelector('#hamburger .a').style.top = '32px';
-    document.querySelector('#hamburger .c').style.transform = 'rotate(130deg)';
-    document.querySelector('#hamburger .c').style.top = '32px';
+    navber.style.transform = "scale(1)";
+    hamburger.style.boxShadow = "unset";
+    lineB.style.transform = "rotate(225deg)";
+    lineA.style.transform = "rotate(130deg)";
+    lineA.style.top = "32px";
+    lineC.style.transform = "rotate(130deg)";
+    lineC.style.top = "32px";
 
-    document.querySelector('#navlink').addEventListener('click', function (e) {
-
-      if (e.target.classList.contains('link')) {
-        navber.style.transform = 'scale(0)';
-        document.querySelector('#hamburger .b').style.transform = 'unset';
-        document.querySelector('#hamburger .a').style.transform = 'unset';
-        document.querySelector('#hamburger .a').style.top = '20px';
-        document.querySelector('#hamburger .c').style.transform = 'unset';
-        document.querySelector('#hamburger .c').style.top = '44px';
+    document.querySelector("#navlink").addEventListener("click", function (e) {
+      if (e.target.classList.contains("link")) {
+        navber.style.transform = "scale(0)";
+        lineB.style.transform = "unset";
+        lineA.style.transform = "unset";
+        lineA.style.top = "20px";
+        lineC.style.transform = "unset";
+        lineC.style.top = "44px";
       }
-
-    })
+    });
   }
 });
